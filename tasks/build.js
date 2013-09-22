7222c42eb21cba9af7304f616f1c95b8156c3917
clement <maboiteaspam@wanadoo.fr>
2013-09-22 17:11:14 +0800

    commit

--- tasks/build.js
+++ tasks/build.js
@@ -36,64 +36,63 @@ module.exports = function(grunt) {
 
     grunt.registerTask("phantomizer-build", "", function () {
 
-        var options = this.options()
+        var options = this.options();
+        var grunt_config = grunt.config.get();
 
-        var urls = options.urls
-        var build_target = options.build_target
-        var clean_dir = options.clean_dir
+        var build_target = options.build_target;
+        var clean_dir = options.clean_dir;
 
         for( var n in clean_dir ){
             deleteFolderRecursive(clean_dir[n])
             grunt.file.mkdir(clean_dir[n])
         }
 
-        var tasks = []
-        for( var n in urls ){
-            tasks.push("phantomizer-html-jitbuild:"+build_target+":"+urls[n])
+        var tasks = [];
+        if( options.urls ){
+            for( var n in options.urls ){
+                tasks.push("phantomizer-html-jitbuild:"+build_target+":"+options.urls[n])
+            }
+        }else if (grunt_config.routing){
+            for( var n in grunt_config.routing ){
+                var route = grunt_config.routing[n];
+                var url = route.template;
+                tasks.push("phantomizer-html-jitbuild:"+build_target+":"+url)
+            }
         }
         grunt.task.run( tasks );
 
     });
 
-    grunt.registerTask("phantomizer-export-build", "", function () {
+    grunt.registerMultiTask("phantomizer-export-build", "", function () {
         var done = this.async();
 
-        var options = this.options()
+        var options = this.options();
 
-        var paths = options.paths
-        var build_dir = options.out_dir
-        var export_dir = options.export_dir
-        var rm_dir = options.rm_dir
-        var rm_files = options.rm_files
+        var paths = options.paths;
+        var copy_patterns = options.copy_patterns;
+        var export_dir = options.export_dir;
+        var rm_dir = options.rm_dir;
+        var rm_files = options.rm_files;
 
-        grunt.file.mkdir( export_dir )
+        grunt.log.ok("exporting to "+export_dir );
+        grunt.file.mkdir( export_dir );
 
         for( var n in paths ){
-            copy_recusive(paths[n], "**/*.html", export_dir)
-            copy_recusive(paths[n], "**/*.htm", export_dir)
-
-            copy_recusive(paths[n], "**/*.js", export_dir)
-            copy_recusive(paths[n], "**/*.css", export_dir)
-
-            copy_recusive(paths[n], "**/*.jpeg", export_dir)
-            copy_recusive(paths[n], "**/*.jpg", export_dir)
-            copy_recusive(paths[n], "**/*.png", export_dir)
-            copy_recusive(paths[n], "**/*.gif", export_dir)
-
-            copy_recusive(paths[n], "**/*.pdf", export_dir)
-
-            copy_recusive(paths[n], "**/*.xml", export_dir)
-            copy_recusive(paths[n], "**/*.json", export_dir)
-            copy_recusive(paths[n], "**/*.jsonp", export_dir)
+            for( var k in copy_patterns ){
+                grunt.log.ok("copying "+paths[n]+""+copy_patterns[n]);
+                copy_recusive(paths[n], copy_patterns[k], export_dir);
+            }
         }
 
 
         for( var n in rm_dir ){
-            deleteFolderRecursive(rm_dir[n])
+            grunt.log.ok("deleting "+rm_dir[n]);
+            deleteFolderRecursive(rm_dir[n]);
         }
         for( var n in rm_files ){
+            grunt.log.ok("deleting "+rm_files[n]);
             if( fs.existsSync(rm_files[n]) )
-                fs.unlinkSync(rm_files[n])
+                fs.unlinkSync(rm_files[n]);
         }
 
         done();
